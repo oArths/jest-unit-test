ab959f83cdbe0b0d15fe480452f349c038f5a4e2
Arthur <arthurferreira1417@gmail.com>
2024-06-18 20:22:49 -0300

    Feat: faznedo teste unitario da função sum

---
+++ src/test/sum.js
@@ -0,0 +1,5 @@
+ function Sum(a, b) {
+    return a + b;
+  }
+  
+  module.exports = Sum
---
+++ src/test/sum.test.js
@@ -0,0 +1,5 @@
+const sum = require('./sum')
+
+test('teste a função de soma', () => {
+    expect(sum(1,4)).toBe(5)
+})
