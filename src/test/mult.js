baf8e2cec5d9a905aefa2d8756f9c7a6864f0d01
Arthur <arthurferreira1417@gmail.com>
2024-06-18 22:07:02 -0300

    Feat: fazendo teste unitario da classse de mult

---
+++ src/test/mult.js
@@ -0,0 +1,4 @@
+export function Mult(a, b) {
+    return a * b;
+  }
+  
---
+++ src/test/mult.test.js
@@ -0,0 +1,6 @@
+import {Mult} from "./mult"
+
+
+test('teste a função de subtração', () => {
+    expect(Mult(4,2)).toBe(8)
+})
